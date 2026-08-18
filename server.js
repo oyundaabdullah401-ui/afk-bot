ae20e2266bc9439872ac82d03f4db83381630f5c
oyundaabdullah401-ui <oyundaabdullah401@gmail.com>
2026-08-18 11:25:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function createBot() {
     const bot = mineflayer.createBot({
     host: 'mutgunmc.mcsh.io', // Örn: sunucum.aternos.me (Sonunda port olmayacak!)
     port: 25565,                 // Aternos'un verdiği 5 haneli port numarası
-    username: 'AFK_Bot_724',
+    username: 'MutGunAFK',
     version: '1.21.4'            // false yerine sunucunuzun sürümünü yazın (Örn: '1.20.1', '1.16.5')
   });
   
